9c5861826959dcce5e2ee0ce8d48923c6a8c1c77
Gennady <gennady.hieber@gmail.com>
2015-11-01 01:11:32 +0300

    chapter two end

--- Assets/Scripts/ProceduralTexture.cs
+++ Assets/Scripts/ProceduralTexture.cs
@@ -35,27 +35,57 @@ public class ProceduralTexture : MonoBehaviour {
 
     private Texture2D GenerateGradient()
     {
-        return GenerateCircleGradient();
-    }
-
-    private Texture2D GenerateCircleGradient()
-    {
         Texture2D proceduralTexture = new Texture2D(widthHieght, widthHieght);
 
-        Vector2 centerPixelPosition = centerPosition*widthHieght;
+        Vector2 centerPixelPosition = centerPosition * widthHieght;
 
         for (int x = 0; x < widthHieght; x++)
         {
             for (int y = 0; y < widthHieght; y++)
             {
-                Vector2 currentPosition = new Vector2(x, y);
-                float pixelDistance = Vector2.Distance(currentPosition, centerPixelPosition)/(widthHieght * 0.5f);
-                pixelDistance = Mathf.Abs(1 - Mathf.Clamp(pixelDistance, 0f, 1f));
-                Color pixelColor = new Color(pixelDistance, pixelDistance, pixelDistance, 1.0f);
-                proceduralTexture.SetPixel(x, y, pixelColor);
+                proceduralTexture.SetPixel(x, y, GenerateScalarMultiplierWithAxis(x, y, centerPixelPosition));
             }
         }
         proceduralTexture.Apply();
         return proceduralTexture;
+    }
+
+    private Color GenerateCircleGradient(int x, int y, Vector2 centerPixelPosition)
+    {
+        Vector2 currentPosition = new Vector2(x, y);
+        float pixelDistance = Vector2.Distance(currentPosition, centerPixelPosition)/(widthHieght * 0.5f);
+        pixelDistance = Mathf.Abs(1 - Mathf.Clamp(pixelDistance, 0f, 1f));
+        return new Color(pixelDistance, pixelDistance, pixelDistance, 1.0f);
+    }
+
+    private Color GenerateCirclesGradient(int x, int y, Vector2 centerPixelPosition)
+    {
+        Vector2 currentPosition = new Vector2(x, y);
+        float pixelDistance = Vector2.Distance(currentPosition, centerPixelPosition) / (widthHieght * 0.5f);
+        pixelDistance = Mathf.Abs(1 - Mathf.Clamp(pixelDistance, 0f, 1f));
+        pixelDistance = (Mathf.Sin(pixelDistance*30.0f)*pixelDistance);
+        return new Color(pixelDistance, pixelDistance, pixelDistance, 1.0f);
+    }
+
+    private Color GenerateScalarMultiplier(int x, int y, Vector2 centerPixelPosition)
+    {
+        Vector2 currentPosition = new Vector2(x, y);
+        Vector2 pixelDirection = centerPixelPosition - currentPosition;
+        pixelDirection.Normalize();
+        float rightDirection = Vector2.Dot(pixelDirection, Vector2.right);
+        float leftDirection = Vector2.Dot(pixelDirection, -Vector2.right);
+        float upDirection = Vector2.Dot(pixelDirection, Vector2.up);
+        return new Color(rightDirection, leftDirection, upDirection, 1.0f);
+    }
+
+    private Color GenerateScalarMultiplierWithAxis(int x, int y, Vector2 centerPixelPosition)
+    {
+        Vector2 currentPosition = new Vector2(x, y);
+        Vector2 pixelDirection = centerPixelPosition - currentPosition;
+        pixelDirection.Normalize();
+        float rightDirection = Vector2.Angle(pixelDirection, Vector2.right)/360;
+        float leftDirection = Vector2.Angle(pixelDirection, -Vector2.right)/360;
+        float upDirection = Vector2.Angle(pixelDirection, Vector2.up)/360;
+        return new Color(rightDirection, leftDirection, upDirection, 1.0f);
     }
 }
---
+++ Assets/Shaders/Chapter2/Chapter2LevelEffect.shader
@@ -0,0 +1,68 @@
+﻿Shader "CookbookShaders/Chapter2/Chapter2LevelEffect" 
+{
+	Properties
+	{
+		_MainTex	("Base (RGB)", 2D)				= "white" {}
+		_inBlack	("Input Black", Range(0, 255))	= 0
+		_inGamma	("Input Gamma", Range(0, 2))	= 1.61
+		_inWhite	("Input White", Range(0,255))	= 255
+
+		_outWhite	("Output White", Range(0, 255))	= 255
+		_outBlack	("Output Black", Range(0, 255))	= 0
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+		
+		CGPROGRAM
+		#pragma surface surf Lambert
+
+		sampler2D _MainTex;
+		float _inBlack;
+		float _inGamma;
+		float _inWhite;
+		float _outWhite;
+		float _outBlack;
+
+		struct Input
+		{
+			float2 uv_MainTex;
+		};
+
+		float GetPixelChanelLevel(float pixelColor)
+		{
+			float pixelResult;
+			pixelResult = (pixelColor * 255.0);
+			pixelResult = max(0, pixelResult - _inBlack);
+			pixelResult = saturate(pow(pixelResult / (_inWhite - _inBlack), _inGamma));
+			pixelResult = (pixelResult * (_outWhite - _outBlack) + _outBlack) / 255.0;
+			return pixelResult;
+		}
+
+		float3 GetPixelLevel(float3 pixel)
+		{
+			float3 result;
+			result.r = GetPixelChanelLevel(pixel.r);
+			result.g = GetPixelChanelLevel(pixel.g);
+			result.b = GetPixelChanelLevel(pixel.b);
+			return result;
+		}
+
+		void surf(Input IN, inout SurfaceOutput o)
+		{
+			half4 c = tex2D(_MainTex, IN.uv_MainTex);
+
+			float outRPixel;
+			outRPixel = (c.r * 255.0);
+			outRPixel = max(0, outRPixel - _inBlack);
+			outRPixel = saturate(pow(outRPixel / (_inWhite - _inBlack), _inGamma));
+			outRPixel = (outRPixel * (_outWhite - _outBlack) + _outBlack) / 255.0;
+
+			o.Albedo = GetPixelLevel(c.rgb);
+			o.Alpha = c.a;
+		}
+		ENDCG
+	} 
+	FallBack "Diffuse"
+}
