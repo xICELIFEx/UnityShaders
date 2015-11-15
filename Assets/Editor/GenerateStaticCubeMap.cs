8be88b49468af9c14ed18a02f44b8a84e71effb9
Gennady <gennady.hieber@gmail.com>
2015-11-15 21:50:14 +0300

    Before frenel

---
+++ Assets/Editor/GenerateStaticCubeMap.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class GenerateStaticCubeMap : ScriptableWizard
+{
+    [SerializeField]
+    private Transform _renderPosition;
+
+    [SerializeField]
+    private Cubemap _cubemap;
+
+    public void OnWizardUpdate()
+    {
+        helpString = "Select transform to render from and cubemap to render into";
+        isValid = (_renderPosition != null && _cubemap != null);
+    }
+
+    public void OnWizardCreate()
+    {
+        GameObject tempCamera = new GameObject("CabeCam", typeof(Camera));
+        tempCamera.transform.position = _renderPosition.position;
+        tempCamera.transform.rotation = Quaternion.identity;
+
+        tempCamera.camera.RenderToCubemap(_cubemap);
+
+        Destroy(tempCamera);
+    }
+
+    [MenuItem("CookBook/Render Cubemap")]
+    public static void RenderCubemap()
+    {
+        ScriptableWizard.DisplayWizard("Render CubeMap", typeof (GenerateStaticCubeMap), "Render!");
+    }
+}
